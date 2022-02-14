f2ec0794eca3e56a0d9b7ac381488e749625d4f7
tiagoeuf <eufr4sia@gmail.com>
2022-02-14 17:33:22 +0000

    route para componentNav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { NavComponent } from "./components/nav/nav.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NavComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
