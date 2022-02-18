3090e6493e93a4d3be526e9ce33ec036f2733bd6
tiagoeuf <eufr4sia@gmail.com>
2022-02-18 17:50:56 +0000

    CRUD para cliente

---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  constructor(private http: HttpClient) { }
+
+
+  findById(id: any): Observable<Cliente> {
+    return this.http.get<Cliente>(`${API_CONFIG.baseUrl}/cliente/${id}`)
+  }
+
+  findAll(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(`${API_CONFIG.baseUrl}/cliente`);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(`${API_CONFIG.baseUrl}/cliente`, cliente);
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${API_CONFIG.baseUrl}/cliente/${cliente.id}`, cliente);
+  }
+
+  delete(id: any): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${API_CONFIG.baseUrl}/cliente/${id}`);
+  }
+}
+
